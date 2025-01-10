86a87632713c53638448bbfd1bfbd456db74432f
Shubham BHoilkar <sandesh.mo@neuralit.com>
2025-01-10 16:34:07 +0530

    Redis initialized

--- user_microservice/main.py
+++ user_microservice/main.py
@@ -8,6 +8,9 @@ from logging.handlers import RotatingFileHandler
 import configparser
 import redis
 
+def get_redis_client():
+    return redis.StrictRedis(host="localhost", port=6379, decode_response=True)
+
 
 config = configparser.ConfigParser()
 config.read('/home/neuralit/shubham_workarea/python/microservice_application/config.ini')
@@ -38,17 +41,51 @@ logger = setup_logging(log_file_path)
 # try:
 #     redis_client =redis.Redis(host= redis_host, port =redis_port, db= redis_db,)
 
+redis_client = get_redis_client()
+
 app = FastAPI()
 
 @app.get("/")
 def main_page():
     return {"Welcome":"Here comes your demo home page"}
 
+@app.on_event("startup")
+def startup_event():
+    try:
+        redis_client.ping()
+        logger.info("Connect to Redis successfully.")
+    except redis.ConnectionError as e:
+        logger.error(f"Failed to connect to Redis: {e}")
+        raise
+
+@app.on_event("shoutdown")
+def shutdown_event():
+    try:
+        redis_client.close()
+        logger.info("Redis connection closed.")
+    except redis.ConnectionError as e:
+        logger.error(f"Failed to close Redis connection. {e}")
+        raise
+def rate_limit(ip:str, limit: int = 5, window : int =60):
+    key = f"rate_limit:{ip}"
+    current_count = redis_client.incr(key)
+    if current_count == 1:
+        redis_client.expire(key, window)
+    if current_count > limit:
+         return False
+    return True
+
 @app.post("/register")
 def register_user(user: create_user):
     try:
+        ip = "user_ip_placeholder"
+        if not rate_limit(ip):
+            return JSONResponse(status_code= 429,
+                                content={"status":"failure",
+                                         "message":"Rate limit exceeded. Try again later."})
         result = register_user_caller(user, logger)
         if result:
+            redis_client.set(f"user:{user.user_id}",user.json(),ex =3600)
             return JSONResponse(
                 status_code=201,
                 content ={"status":"success","message":user})
@@ -56,7 +93,7 @@ def register_user(user: create_user):
             return JSONResponse(
                 status_code=400,
                 content ={"status":"failure", "message": "User Creation request not sent to NSQ."})
-            
+
     except Exception as e:
         logger.error(f"Error during user registration: {e}",exc_info =True)
         raise HTTPException(status_code= 500, detail="Internal Server Error.")
@@ -64,6 +101,12 @@ def register_user(user: create_user):
 @app.get("/get_user_details/{user_id}")
 def get_user_details(user_id: int):
     try:
+        cached_data = redis_client.get(f"user:{user_id}")
+        if cached_data:
+            return JSONResponse(
+                status_code=200,
+                content={"status": "success", "data": cached_data}
+            )
         records = view_records_logic(user_id, logger)
         
         if records:
@@ -95,11 +138,6 @@ def update_user_details(user_id: int,user: update_user):
     except Exception as e:
         logger.error(f"Error during the update: {e}",exc_info =True)
         raise HTTPException(status_code = 500, detail="Internal Server Error.")
-'''
-sudo apt-get update
-sudo apt-get install iputils-ping
-ping 10.10.7.64
-'''
 
 @app.delete("/delete_user/{user_id}")
 def delete_user(user_id: int):
@@ -122,6 +160,4 @@ def delete_user(user_id: int):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app=app,host=host, port=eval(port))
-
-#curl -d "Hello, NSQ!" "http://10.10.7.64:4151/pub?topic=update-user"
-#curl -d "Hello, NSQ!" "http://10.10.7.64:4151/pub?topic=delete-user"
+    
--- user_microservice/user_consumer.py
+++ user_microservice/user_consumer.py
@@ -2,14 +2,33 @@ from user_api_function import register_user_logic , update_user_logic , delete_u
 import nsq
 import json
 import configparser
+import logging
+from logging.handlers import RotatingFileHandler
 
 config = configparser.ConfigParser()
 config.read('/home/neuralit/shubham_workarea/python/microservice_application/config.ini')
+log_file_path = config['Log']['file_path']
+
+def setup_logging(file_path):
+    logger = logging.getLogger('user_microservice')
+    logger.setLevel(logging.DEBUG)
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=5*1024*1024, backupCount=5)
+    file_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+logger = setup_logging(log_file_path)
+
 
 nsq_address = config['NSQ']['host']
 nsq_port =config['NSQ']['port']
 
-def nsq_subscription_handler(queue_name,channel_name, callback,log):
+def nsq_subscription_handler(queue_name, callback,log):
     """
     Function to handle NSQ topic subscription.    
     Args:
@@ -29,20 +48,17 @@ def nsq_subscription_handler(queue_name,channel_name, callback,log):
             return message.requeue()
 
     # Create an NSQ reader to subscribe to the topic.
-    reader = nsq.Reader(
-        message_handler=message_handler,    # Handle the messages.
+    reader = nsq.Reader(message_handler=message_handler,    # Handle the messages.
         topic=queue_name,            # The topic to subscribe to (change this as needed).
-        channel=channel_name,                 # Use the provided queue name for the subscription.
-        nsqd_tcp_addresses=[f'{nsq_address}:{nsq_port}'],  # Provide NSQ server address.
+        channel="demo",                 # Use the provided queue name for the subscription.
+        nsqd_tcp_addresses=[f'{nsq_address}:{4151}'],  # Provide NSQ server address.    
         max_in_flight=10 )                   # Max number of messages to be processed in parallel.
 
 
     # Run the reader to listen for incoming messages.
     try:
-        log.info(f"Subscribing to queue: {queue_name} on topic: 'your_topic_name'")
+        # log.info(f"Subscribing to queue: {queue_name} on topic: 'your_topic_name'")
         nsq.run()
-    except KeyboardInterrupt:
-        log.info("Subscription interrupted by user.")
     except Exception as e:
         log.error(f"Error during NSQ subscription: {e}",exc_info=True)
         
@@ -96,14 +112,19 @@ if __name__ == "__main__":
     # register_nsq_consumer()
     # read system argument and subscription to a topic and assign callback function
     queue_name = "register-user"
-    channel_name = "RegisterChannel"
-
-    if queue_name == "register-user" and channel_name =="RegisterChannel":
-        nsq_subscription_handler(queue_name,channel_name, register_user, None)
-    elif queue_name == "-update_record" and channel_name== "UpdateChannel":
-        nsq_subscription_handler(queue_name,channel_name, update_user_data, None)
-    elif queue_name == "delete-user_record" and channel_name== "DeleteChannel":
-        nsq_subscription_handler(queue_name, channel_name,delete_user_data, None)
-    else:
-#        log.error(f"{queue_name} not found")
-        print(f"Topic: {queue_name} and Channel: {channel_name} not found.")
+#    channel_name = "RegisterChannel"
+    def consumer_topic():
+        queue_name = input("Enter the topic name: ")
+        if queue_name == "register-user" :
+            nsq_subscription_handler(queue_name, register_user, logger)
+        elif queue_name == "update-user" :
+            nsq_subscription_handler(queue_name, update_user_data, logger)
+        elif queue_name == "delete-user":
+            nsq_subscription_handler(queue_name,delete_user_data, logger)
+        else:
+    #        log.error(f"{queue_name} not found")
+            print(f"Topic: {queue_name} not found.")
+        return True
+    
+    consumer_topic()
+        
